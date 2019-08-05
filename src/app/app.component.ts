a59ba1da0974578f2d644735a2149211a7dfdf7f
Pankaj Parkar <parkarpankaj3@gmail.com>
2019-08-05 09:08:21 +0530

    Cleaned App Module and app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,62 +1,12 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { Subscription } from 'rxjs';
-
-import { AppService } from './app.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ms-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit, OnDestroy {
-  singleSelectOptions;
-  propertyMapOptions;
-  multipleSelectOptions;
-  groupingOptions;
-  observableOptions;
-  customTemplateOptions;
-  singleSelectOptionsSubscription: Subscription;
-  multipleSelectOptionsSubscription: Subscription;
-  groupingOptionsSubscription: Subscription;
-
-  constructor(private appService: AppService) {}
-
-  singleSelect: any = { id: 1, name: 'a' };
-  propertyMapValue: any = { id: 2, name: 'b' };
-  mutlipleSelect = new FormControl([{ id: 1, name: 'Test 1' }, { id: 2, name: 'Test 2' }]);
-  groupingSupport = new FormControl([{ id: 1, name: 'Test 1' }]);
-  withObservable = new FormControl([{ id: 1, name: 'Test 1' }]);
-  customTemplateSelect: any = new FormControl([{ id: 1, name: 'a' }, { id: 2, name: 'b' }]);
-
-  propertyMap = {
-    EmpId: 'id',
-    EmpName: 'name'
-  };
+export class AppComponent implements OnInit {
 
   ngOnInit(): void {
-    // Retrieving data for dropdown
-    this.singleSelectOptionsSubscription = this.appService.getSingleSelectOptions().subscribe(data => {
-      this.propertyMapOptions = [...data];
-      this.singleSelectOptions = [...data];
-      this.customTemplateOptions = [...data];
-    });
-    this.multipleSelectOptionsSubscription = this.appService
-      .getMultipleSelectOptions()
-      .subscribe(data => (this.multipleSelectOptions = data));
-    this.groupingOptionsSubscription = this.appService
-      .getGroupingOptions()
-      .subscribe(data => (this.groupingOptions = data));
-    this.observableOptions = this.appService.getObservableOptions();
-  }
-
-  genericEvent($event, eventName) {
-    console.log('Event fired ' + eventName, $event);
-  }
-
-  ngOnDestroy() {
-    this.singleSelectOptionsSubscription.unsubscribe();
-    this.multipleSelectOptionsSubscription.unsubscribe();
-    this.groupingOptionsSubscription.unsubscribe();
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,82 +4,18 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { NgxMultiselectModule } from '@ngx-lib/multiselect';
-import { CustomMaterialModule } from './custom-material/custom-material.module';
-import { AppRoutingModule } from './app-routing.module';
-
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { BannerComponent } from './banner/banner.component';
-import { HomeComponent } from './home/home.component';
-import { SetupComponent } from './setup/setup.component';
-import { TitleComponent } from './title/title.component';
-import { ContentComponent } from './content/content.component';
-import { NoteComponent } from './note/note.component';
-import { DocComponent } from './doc/doc.component';
-import { DemoContainerComponent } from './demo-container/demo-container.component';
-import { DemoComponent } from './demo/demo.component';
-import { CodeViewComponent } from './code-view/code-view.component';
-import { LayoutRendererComponent } from './layout-renderer/layout-renderer.component';
-
-import { PropertyMapComponent } from './demos/doc-demos/propertymap/propertymap.component';
-import { LargeVolumeDataComponent } from './demos/testing-demos/large-volume-data/large-volume-data.component';
-import { EventsDemoComponent } from './demos/doc-demos/events/events.component';
-import { ThemeComponent } from './demos/doc-demos/theme/theme.component';
-import { GettingStartedComponent } from './demos/doc-demos/getting-started/getting-started.component';
-import { SingleSelectComponent } from './demos/doc-demos/single-select/single-select.component';
-import { TemplateDrivenComponent } from './demos/doc-demos/template-driven/template-driven.component';
-import { ModelDrivenComponent } from './demos/doc-demos/model-driven/model-driven.component';
-import { ObservableAsyncComponent } from './demos/doc-demos/observable-async/observable-async.component';
-import { ObservableAsyncServiceComponent } from './demos/doc-demos/observable-async-service/observable-async-service.component';
-import { ObservableSubscribeComponent } from './demos/doc-demos/observable-subscribe/observable-subscribe.component';
-import { ObservableDynamicUpdateComponent } from './demos/doc-demos/observable-dynamic-update/observable-dynamic-update.component';
-import { CustomTemplateComponent } from './demos/doc-demos/custom-template/custom-template.component';
-import { GroupingComponent } from './demos/doc-demos/grouping/grouping.component';
-import { EnableDisableComponent } from './demos/doc-demos/enable-disable/enable-disable.component';
-import { DemoShowcaseComponent } from './demos/testing-demos/demo-showcase/demo-showcase.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PropertyMapComponent,
-    LargeVolumeDataComponent,
-    NavbarComponent,
-    BannerComponent,
-    HomeComponent,
-    SetupComponent,
-    TitleComponent,
-    ContentComponent,
-    NoteComponent,
-    DocComponent,
-    DemoContainerComponent,
-    DemoComponent,
-    CodeViewComponent,
-    LayoutRendererComponent,
-    EventsDemoComponent,
-    ThemeComponent,
-    GettingStartedComponent,
-    SingleSelectComponent,
-    TemplateDrivenComponent,
-    ModelDrivenComponent,
-    ObservableAsyncComponent,
-    ObservableAsyncServiceComponent,
-    ObservableSubscribeComponent,
-    ObservableDynamicUpdateComponent,
-    CustomTemplateComponent,
-    GroupingComponent,
-    EnableDisableComponent,
-    DemoShowcaseComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    NgxMultiselectModule,
     BrowserAnimationsModule,
-    CustomMaterialModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
